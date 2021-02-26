e04f998cf1759b99a7fdec90949622655991eae1
Jahl Herapath <jahl.herapath@gmail.com>
2021-02-26 16:30:00 +0100

    Setup header and footer

--- web/components/alert.js
+++ web/components/alert.js
@@ -4,15 +4,10 @@ import { EXAMPLE_PATH } from "../lib/constants";
 
 export default function Alert({ preview }) {
   return (
-    <div
-      className={cn("border-b", {
-        "bg-accent-7 border-accent-7 text-white": preview,
-        "bg-accent-1 border-accent-2": !preview,
-      })}
-    >
+    <div>
       <Container>
         <div className="py-2 text-center text-sm">
-          {preview ? (
+          {preview && (
             <>
               This page is a preview.{" "}
               <a
@@ -20,19 +15,8 @@ export default function Alert({ preview }) {
                 className="underline hover:text-cyan duration-200 transition-colors"
               >
                 Click here
-              </a>{" "}
-              to exit preview mode.
-            </>
-          ) : (
-            <>
-              The source code for this blog is{" "}
-              <a
-                href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-                className="underline hover:text-success duration-200 transition-colors"
-              >
-                available on GitHub
               </a>
-              .
+              to exit preview mode.
             </>
           )}
         </div>
--- web/components/more-listings.js
+++ web/components/more-listings.js
@@ -3,9 +3,6 @@ import ListingPreview from "./listing-preview";
 export default function MoreListings({ listings }) {
   return (
     <section>
-      <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
-        More Listings
-      </h2>
       <div className="grid grid-cols-1 md:grid-cols-4 gap-6 mb-32">
         {listings.map((listing) => (
           <div>
