ca7cb1d435fecf71af88de79538ae222e94d747c
Jahl Herapath <jahl.herapath@gmail.com>
2021-03-03 08:20:00 +0100

    Build fallback for category page

--- web/pages/category/[slug].js
+++ web/pages/category/[slug].js
@@ -20,6 +20,12 @@ export default function Categories({
 }) {
   const router = useRouter();
 
+  // If the page is not yet generated, this will be displayed
+  // initially until getStaticProps() finishes running
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+
   const currentSlug = router.query.slug;
 
   const currentSlugName = allCategories.find((c) => c.slug === currentSlug);
