6b905f225adac280794d44010a243a42233823dd
Jahl Herapath <jahl.herapath@gmail.com>
2021-03-08 17:23:59 +1100

    Category active state

--- web/components/categories.js
+++ web/components/categories.js
@@ -1,11 +1,28 @@
 import Container from "../components/container";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Categories({ categories }) {
+  const router = useRouter();
+
+  const currentSlug = router.query.slug;
+
   return (
     <section>
       <Container>
         <div className="flex flex-wrap justify-center">
+          <Link href="/">
+            <a
+              style={{ margin: "2px" }}
+              className={`transition duration-200 ease text-sm inline-flex hover:bg-primary hover:text-white rounded-full font-medium px-4 py-1 ${
+                currentSlug === undefined
+                  ? "bg-primary text-white "
+                  : "bg-black bg-opacity-5 text-black text-opacity-50 "
+              }`}
+            >
+              All
+            </a>
+          </Link>
           {categories.map((category, index) => (
             <Link
               key={index}
@@ -14,7 +31,11 @@ export default function Categories({ categories }) {
             >
               <a
                 style={{ margin: "2px" }}
-                className="transition duration-200 ease text-sm inline-flex bg-black bg-opacity-5 hover:bg-primary hover:text-white rounded-full text-black text-opacity-50 font-medium px-4 py-1"
+                className={`transition duration-200 ease text-sm inline-flex hover:bg-primary hover:text-white rounded-full font-medium px-4 py-1 ${
+                  currentSlug === category.slug
+                    ? "bg-primary text-white "
+                    : "bg-black bg-opacity-5 text-black text-opacity-50 "
+                }`}
               >
                 {category.name}
               </a>
