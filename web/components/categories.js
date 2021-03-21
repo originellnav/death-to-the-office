72b9e441ff863dc69f9141a4fadb1eff833783db
Jahl Herapath <jahl.herapath@gmail.com>
2021-03-22 08:16:33 +1100

    Fixing some css stuff and sanity deploy

--- web/components/categories.js
+++ web/components/categories.js
@@ -11,14 +11,14 @@ export default function Categories({ categories }) {
   return (
     <section>
       <Container>
-        <div className="flex flex-wrap justify-center">
+        <div className="flex flex-wrap justify-center w-full md:w-full mx-auto">
           <Link href="/">
             <a
               style={{ margin: "2px" }}
               className={`font-body transition duration-200 ease text-sm inline-flex hover:bg-primary hover:text-white rounded-full font-medium px-4 py-1 ${
                 currentSlug === undefined
                   ? "bg-primary text-white "
-                  : "bg-black bg-opacity-5 text-black text-opacity-50 "
+                  : "bg-black bg-opacity-5 text-black text-opacity-50"
               }`}
             >
               All
