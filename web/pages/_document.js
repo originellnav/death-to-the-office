4b6710b1f8c585b043ed833e15c38c11b04f3852
Jahl Herapath <jahl.herapath@gmail.com>
2021-05-12 14:52:47 +1000

    Updated Google Analytics

--- web/pages/_document.js
+++ web/pages/_document.js
@@ -4,7 +4,25 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=G-7H7WKTLLJE`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-7H7WKTLLJE', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
